656cb8a56dd3f1488160b31fb89d13c19892e290
Théo Zanchi <tzanchi@student.42berlin.de>
2024-10-29 19:44:09 +0100

    first script, does not work because of gpg utils missing

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade && apt-get install -y curl
+
+curl -sfL https://get.docker.com/ | sh -
+
+sudo apt-get update && sudo apt-get install -y kubectl
