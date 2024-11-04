c34959bd30b0e8d1e919c0cf1b5760c3e5b841c6
Théo Zanchi <tzanchi@student.42berlin.de>
2024-11-04 17:55:30 +0100

    added Argo CD to repo

--- p3/setup.sh
+++ p3/setup.sh
@@ -31,3 +31,8 @@ sudo apt-get install -y kubectl
 
 # Installing k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# Installing Argo CD CLI
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
