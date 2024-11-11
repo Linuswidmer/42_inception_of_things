b36dcb9864e87236e703a4c6e614ac91adbfbd57
mdarbois <mdarbois@student.42berlin.de>
2024-11-11 18:36:12 +0100

    added more clean rule and killing the port forwarding 888

--- p3/scripts/launch_dev.sh
+++ p3/scripts/launch_dev.sh
@@ -3,8 +3,11 @@ echo "The Argo CD initial admin password is: $ARGOCD_PASSWORD"
 
 PASS="admin"
 
-argocd --port-forward --port-forward-namespace=argocd login --username=admin --password=$ARGOCD_PASSWORD
+#argocd --port-forward --port-forward-namespace=argocd login --username=admin --password=$ARGOCD_PASSWORD
 # argocd --port-forward --port-forward-namespace=argocd account update-password --current-password $ARGOCD_PASSWORD --new-password $PASS
+#sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+sudo argocd app sync wil-playground --server localhost:8080 --insecure
 
 echo "Applying the application.yaml file..."
 sudo kubectl apply -f application.yaml
@@ -22,4 +25,10 @@ sudo kubectl set image deployment/wil-playground wil-playground=wil42/playground
 
 sudo argocd app sync wil-playground
 
+#if sudo lsof -i :8888; then
+#  echo "Port 8888 is in use. Killing the process..."
+#  sudo fuser -k 8888/tcp
+#fi
+
+sudo fuser -k 8888/tcp
 sudo kubectl port-forward svc/wil-playground 8888:8888 -n dev &
